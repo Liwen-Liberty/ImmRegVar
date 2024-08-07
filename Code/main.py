4cbd63993881f1d656d829524ee16f920cb5ae1a
Liwen Xu <37435209+Liwen-Liberty@users.noreply.github.com>
2024-08-07 10:54:57 +0800

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 18 15:05:58 2024
-
-@author: elizahe
-"""
-
 import numpy as np
 import torch
 from load_data import load_data2
